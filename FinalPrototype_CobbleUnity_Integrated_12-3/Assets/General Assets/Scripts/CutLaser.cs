898acbe82e0924e31af750eacb39a1740d899c25
andrew-vanwie <wanderinwave@gmail.com>
2016-03-26 13:13:33 -0400

    Cutting Laser Animation
    Cutting laser trigger/target animation working.

---
+++ FinalPrototype_CobbleUnity_Integrated_12-3/Assets/General Assets/Scripts/CutLaser.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutLaser : MonoBehaviour {
+
+	public GameObject objectToCut;
+	public GameObject CuttingLaser;
+	public GameObject CuttingLaserStart;
+	public GameObject CuttingLaserEnd;
+	public Canvas promptUI;
+	private bool inRange = false;
+
+	// Use this for initialization
+	void Start () {
+		CuttingLaserStart = GameObject.Find ("LaserStart");
+		CuttingLaserEnd = GameObject.Find ("LaserEnd");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (inRange == true && Input.GetButton ("Fire1")) {
+			//CuttingLaser.GetComponent<Renderer> ().enabled = true;
+			CuttingLaser.SetActive (true);
+			CuttingLaserEnd.transform.position = objectToCut.transform.position;
+			CuttingLaserEnd.transform.LookAt (CuttingLaserStart.transform);
+		} else {
+			CuttingLaser.SetActive (false);
+		}
+
+	}
+		
+
+	void OnTriggerEnter(Collider other){
+
+		if (other.tag == "Player") {
+			promptUI.GetComponent<Canvas> ().enabled = true;
+			inRange = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+
+		if (other.tag == "Player") {
+			promptUI.GetComponent<Canvas> ().enabled = false;
+			inRange = false;
+		}
+	}
+}
